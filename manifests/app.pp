035380b19db4697e8f5d53c4b899340e9504d102
Gabriel Bretschner <software@kanedo.net>
2013-03-06 14:46:42 +0100

    some changes

--- manifests/app.pp
+++ manifests/app.pp
@@ -1,56 +1,17 @@
 # Install wordpress application and its dependencies
 class wordpress::app {
-
+  require boxen::config
   $wordpress_archive = 'wordpress-3.5.1.zip'
-
-  $apache = $::operatingsystem ? {
-    Ubuntu   => apache2,
-    CentOS   => httpd,
-    Debian   => apache2,
-    default  => httpd
-  }
-
-  $phpmysql = $::operatingsystem ? {
-    Ubuntu   => php5-mysql,
-    CentOS   => php-mysql,
-    Debian   => php5-mysql,
-    default  => php-mysql
-  }
-
-  $php = $::operatingsystem ? {
-    Ubuntu   => libapache2-mod-php5,
-    CentOS   => php,
-    Debian   => libapache2-mod-php5,
-    default  => php
-  }
-
-  package { ['unzip',$apache,$php,$phpmysql]:
-    ensure => latest
-  }
-
-  $vhost_path = $apache ? {
-    httpd    => '/etc/httpd/conf.d/wordpress.conf',
-    apache2  => '/etc/apache2/sites-enabled/000-default',
-    default  => '/etc/httpd/conf.d/wordpress.conf',
-  }
-
-  service { $apache:
-    ensure     => running,
-    enable     => true,
-    hasrestart => true,
-    hasstatus  => true,
-    require    => Package[$apache, $php, $phpmysql],
-    subscribe  => File['wordpress_vhost'];
-  }
+  $wordpress_src = "${boxen::config::srcdir}/${name}"
 
   file {
     'wordpress_application_dir':
       ensure  =>  directory,
-      path    =>  '/opt/wordpress',
+      path    =>  $wordpress_src,
       before  =>  File['wordpress_setup_files_dir'];
     'wordpress_setup_files_dir':
       ensure  =>  directory,
-      path    =>  '/opt/wordpress/setup_files',
+      path    =>  "${wordpress_src}/setup_files",
       before  =>  File[
                       'wordpress_php_configuration',
                       'wordpress_themes',
@@ -60,22 +21,22 @@ class wordpress::app {
                       ];
     'wordpress_installer':
       ensure  =>  file,
-      path    =>  "/opt/wordpress/setup_files/${wordpress_archive}",
+      path    =>  "${wordpress_src}/setup_files/${wordpress_archive}",
       notify  =>  Exec['wordpress_extract_installer'],
       source  =>  "puppet:///modules/wordpress/${wordpress_archive}";
     'wordpress_php_configuration':
       ensure     =>  file,
-      path       =>  '/opt/wordpress/wp-config.php',
+      path       =>  "${wordpress_src}/wp-config.php",
       content    =>  template('wordpress/wp-config.erb'),
       subscribe  =>  Exec['wordpress_extract_installer'];
     'wordpress_htaccess_configuration':
       ensure     =>  file,
-      path       =>  '/opt/wordpress/.htaccess',
+      path       =>  "${wordpress_src}/.htaccess",
       source     =>  'puppet:///modules/wordpress/.htaccess',
       subscribe  =>  Exec['wordpress_extract_installer'];
     'wordpress_themes':
       ensure     => directory,
-      path       => '/opt/wordpress/setup_files/themes',
+      path       => "${wordpress_src}/setup_files/themes",
       source     => 'puppet:///modules/wordpress/themes/',
       recurse    => true,
       purge      => true,
@@ -84,48 +45,22 @@ class wordpress::app {
       subscribe  => Exec['wordpress_extract_installer'];
     'wordpress_plugins':
       ensure     => directory,
-      path       => '/opt/wordpress/setup_files/plugins',
+      path       => "${wordpress_src}/setup_files/plugins",
       source     => 'puppet:///modules/wordpress/plugins/',
       recurse    => true,
       purge      => true,
       ignore     => '.svn',
       notify     => Exec['wordpress_extract_plugins'],
       subscribe  => Exec['wordpress_extract_installer'];
-    'wordpress_vhost':
-      ensure   => file,
-      path     => $vhost_path,
-      source   => 'puppet:///modules/wordpress/wordpress.conf',
-      replace  => true,
-      require  => Package[$apache];
     }
 
       exec {
       'wordpress_extract_installer':
         command      => "unzip -o\
-                        /opt/wordpress/setup_files/${wordpress_archive}\
+                        ${wordpress_src}/setup_files/${wordpress_archive}\
                         -d /opt/",
         refreshonly  => true,
         require      => Package['unzip'],
         path         => ['/bin','/usr/bin','/usr/sbin','/usr/local/bin'];
-      'wordpress_extract_themes':
-        command      => '/bin/sh -c \'for themeindex in `ls \
-                        /opt/wordpress/setup_files/themes/*.zip`; \
-                        do unzip -o \
-                        $themeindex -d \
-                        /opt/wordpress/wp-content/themes/; done\'',
-        path         => ['/bin','/usr/bin','/usr/sbin','/usr/local/bin'],
-        refreshonly  => true,
-        require      => Package['unzip'],
-        subscribe    => File['wordpress_themes'];
-      'wordpress_extract_plugins':
-        command      => '/bin/sh -c \'for pluginindex in `ls \
-                        /opt/wordpress/setup_files/plugins/*.zip`; \
-                        do unzip -o \
-                        $pluginindex -d \
-                        /opt/wordpress/wp-content/plugins/; done\'',
-        path         => ['/bin','/usr/bin','/usr/sbin','/usr/local/bin'],
-        refreshonly  => true,
-        require      => Package['unzip'],
-        subscribe    => File['wordpress_plugins'];
   }
 }
