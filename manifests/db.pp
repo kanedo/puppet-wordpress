b0dfd110ae8b7a60a9ef98089c86788f53ddd5f0
Gabriel Bretschner <software@kanedo.net>
2013-03-06 15:23:17 +0100

    changed service notifications

--- manifests/db.pp
+++ manifests/db.pp
@@ -1,11 +1,8 @@
 # Install mysql server and set up wordpress database
 class wordpress::db {
 	include mysql
-	service { 'dev.mysql':
-	    ensure      => running,
-	    enable      => true,
-	    hasrestart  => true,
-	    hasstatus   => true,
-	  }
  	mysql::db { "${wordpress::db_name}": }
+	package { 'mysql':
+	    notify => Service['mysql']
+	  }
 }
